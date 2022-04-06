91ad05da4ccaa5d54033780e6310e0599dcd5a6f
Ignacio Isas <ignacioisas8@gmail.com>
2022-04-06 02:02:42 -0300

    tailwind added

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ export function App() {
   const [selectedImg, setSelectedImg] = useState(Images[0]);
 
   return (
-    <div className="App">
-      <div className="container">
-        <img src={selectedImg} alt="Selected" className="selected" />
-        <div className="img-container">
+    <div className="w-auto min-h-screen bg-slate-800 flex">
+      <div className="m-auto w-auto max-w-screen-sm pt-8 py-0 px-0 pr-0">
+        <img src={selectedImg} alt="Selected" className="w-auto h-auto mx-0 my-auto rounded-md " />
+        <div className="w-auto flex justify-between flex-wrap pt-10 py-0 px-0 pr-0">
+          <div className="grid grid-cols-3 gap-4 w-full cursor-pointer justify-items-center">
           {Images.map((img, index) => (
-            <img
+            <img className="w-40 h-28 mx-0 my-0"
               style={{ border: selectedImg === img ? "4px solid lightblue" : "" }}
               key={index}
               src={img}
@@ -19,6 +20,7 @@ export function App() {
             onClick={() => setSelectedImg(img)}
             />
           ))}
+          </div>
         </div>
       </div>
     </div>
