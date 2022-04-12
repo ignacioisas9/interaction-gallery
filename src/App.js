9b08b1be90d944b667f9c2108159d44b8dd8a4e3
Ignacio Isas <ignacioisas8@gmail.com>
2022-04-12 00:26:53 -0300

    button added

--- src/App.js
+++ src/App.js
@@ -6,22 +6,37 @@ export function App() {
   const [selectedImg, setSelectedImg] = useState(Images[0]);
 
   return (
-    <div className="w-auto min-h-screen bg-slate-800 flex">
-      <div className="m-auto w-auto max-w-screen-sm pt-8 py-0 px-0 pr-0">
-        <img src={selectedImg} alt="Selected" className="w-auto h-auto mx-0 my-auto rounded-md " />
-        <div className="w-auto flex justify-between flex-wrap pt-10 py-0 px-0 pr-0">
-          <div className="grid grid-cols-3 gap-4 w-full cursor-pointer justify-items-center">
-          {Images.map((img, index) => (
-            <img className="w-40 h-28 mx-0 my-0"
-              style={{ border: selectedImg === img ? "4px solid lightblue" : "" }}
-              key={index}
-              src={img}
-              alt="city"
-            onClick={() => setSelectedImg(img)}
-            />
-          ))}
+    <div className="w-auto min-h-screen flex bg-slate-800">
+      <div className="m-auto w-auto max-w-screen-sm pt-8 py-0 px-0 pr-0 text-center">
+        <div className="text-5xl pb-8 font-bold text-slate-50">
+          Elige tu próximo destino
+        </div>
+        <img
+          src={selectedImg}
+          alt="Selected"
+          className="w-auto h-auto mx-0 my-auto rounded-md"
+        />
+        <div className="w-auto flex justify-between flex-wrap pt-10 px-0 pb-10">
+          <div className="grid sm:grid-cols-3 md:grid-cols-4 gap-4 w-full cursor-pointer justify-items-center">
+            {Images.map((img, index) => (
+              <figure>
+                <img
+                  className="w-40 h-28 mx-0 my-0 rounded-md"
+                  style={{
+                    border: selectedImg === img ? "4px solid lightblue" : ""
+                  }}
+                  key={index}
+                  src={img}
+                  alt="city"
+                  onClick={() => setSelectedImg(img)}
+                />
+              </figure>
+            ))}
           </div>
         </div>
+        <button className="btn m-8 btn-xs sm:btn-sm md:btn-md lg:btn-lg">
+          Comprar pasajes
+        </button>
       </div>
     </div>
   );
