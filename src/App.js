5f3408eecedfe18f3beff3b5c8a3cf6c61f8e208
Ignacio Isas <ignacioisas8@gmail.com>
2022-04-12 23:27:06 -0300

    Form added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
+import { Form, Field } from "react-final-form";
 import "./App.css";
 import Images from "./Images";
 
 export function App() {
   const [selectedImg, setSelectedImg] = useState(Images[0]);
 
+  const onSubmit = (e) => {
+    debugger;
+  };
+
+  const validate = (e) => {
+    //debugger
+  };
+
   return (
-    <div className="w-auto min-h-screen flex bg-slate-800">
-      <div className="m-auto w-auto max-w-screen-sm pt-8 py-0 px-0 pr-0 text-center">
+    <div className="w-auto min-h-screen flex bg-slate-800 font-sans">
+      <div className="m-auto w-auto max-w-screen-sm pt-8 py-4 px-0 pr-0 text-center">
         <div className="text-5xl pb-8 font-bold text-slate-50">
           Elige tu próximo destino
         </div>
@@ -23,7 +32,7 @@ export function App() {
                 <img
                   className="w-40 h-28 mx-0 my-0 rounded-md"
                   style={{
-                    border: selectedImg === img ? "4px solid lightblue" : ""
+                    border: selectedImg === img ? "4px solid lightblue" : "",
                   }}
                   key={index}
                   src={img}
@@ -34,9 +43,62 @@ export function App() {
             ))}
           </div>
         </div>
+
         <button className="btn m-8 btn-xs sm:btn-sm md:btn-md lg:btn-lg">
           Comprar pasajes
         </button>
+
+        <div className="text-left text-3xl text-slate-50">
+          <h1 className="py-8">
+            Rellena el formulario para estar al tanto de nuestras ofertas:
+          </h1>
+          <Form
+            onSubmit={onSubmit}
+            validate={validate}
+            render={({ handleSubmit }) => (
+              <form class="w-full" onSubmit={handleSubmit}>
+                <div className="flex mb-4 items-center border-b border-teal-500 py-2">
+                  <label className="text-lime-500">Nombre</label>
+                  <Field
+                    className="w-full appearance-none bg-transparent border-none text-white-600 placeholder:text-slate-700 py-1 px-2 leading-tight focus:outline-none"
+                    type="text"
+                    name="firstName"
+                    component="input"
+                    placeholder="Miguel Ignacio"
+                  />
+                </div>
+                <div className="flex mb-4 items-center border-b border-teal-500 py-2">
+                  <label className="text-lime-500">Apellido</label>
+                  <Field
+                    className="w-full appearance-none bg-transparent border-none text-white-700 placeholder:text-slate-700 py-1 px-2 leading-tight focus:outline-none"
+                    type="text"
+                    name="lastName"
+                    component="input"
+                    placeholder="Isas"
+                  />
+                </div>
+                <div className=" mb-4 items-center border-b border-teal-500 py-2">
+                  <label className=" text-lime-500">Correo electrónico</label>
+                  <Field
+                    className="appearance-none bg-transparent border-none text-white-700 placeholder:text-slate-700 py-1 px-2 leading-tight focus:outline-none"
+                    type="text"
+                    name="email"
+                    component="input"
+                    placeholder="ignacioisas@lab9.com"
+                  />
+                </div>
+                <div className="float-right">
+                  <button
+                    className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+                    type="submit"
+                    >
+                    Enviar
+                  </button>
+                </div>
+              </form>
+            )}
+          />
+        </div>
       </div>
     </div>
   );
